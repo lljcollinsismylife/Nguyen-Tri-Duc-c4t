245a8dcd80da6d3b2cc9136712ac062dbe9f83d8
lljcollinsismylife <ittohanyu@gmail.com>
2018-06-10 01:21:06 +0700

    first commit 2

--- 2a_2.py
+++
@@ -1,8 +0,0 @@
-n = int(input(" Enter a positive interger:"))
-
-factorial = 1
-
-for i in range(n):
-    factorial = factorial * (i +1)
-
-print("The factorial is:", factorial)
---
+++ warmup.py
@@ -0,0 +1,25 @@
+# p = [
+# #     "NAm",
+# #     "Hanoi",
+# #     18,
+# #     "Cau Giay",
+# #     150,
+# #     2,
+# #     "64"
+# # ]
+# #
+
+# person = {}
+# print(person)  key must be a str
+
+person = {
+    "name": "Nam",
+    "age": 18
+}
+# print(person)
+# print(person["l"])
+
+person["location"] = "Hanoi"
+print(person)
+
+person["age"] = 20
